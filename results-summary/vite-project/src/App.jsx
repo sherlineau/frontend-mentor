bc599466c9f0b1f19f14a389da22ad6957a46971
Sherline Au <17630357+sherlineau@users.noreply.github.com>
2023-08-13 17:44:53 -0500

    import data for dynamic content

--- results-summary/vite-project/src/App.jsx
+++ results-summary/vite-project/src/App.jsx
@@ -1,12 +1,32 @@
-
-import './App.css'
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [data, setData] = useState([]);
+  const [average, setAverage] = useState(0);
+
+  useEffect(() => {
+    getData();
+    getAvg();
+  });
+
+  const getData = async () => {
+    fetch("./data.json")
+      .then((res) => res.json())
+      .then((res) => setData(res));
+  };
+
+  const getAvg = () => {
+    let totalScore = 0;
+    data.map((i) => (totalScore += i.score));
+    setAverage(Math.floor(totalScore / data.length));
+  };
+
   return (
     <>
-      <h1>hello</h1>
+      {average}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
