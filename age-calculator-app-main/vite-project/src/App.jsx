ce3fa8c8a43168edca2bf724eab0fd790dc713fe
Sherline Au <17630357+sherlineau@users.noreply.github.com>
2024-01-07 17:19:39 -0600

    changed initial value to allow for placeholder usage

--- age-calculator-app-main/vite-project/src/App.jsx
+++ age-calculator-app-main/vite-project/src/App.jsx
@@ -2,25 +2,26 @@ import { useState } from 'react'
 import icon_arrow from './assets/images/icon-arrow.svg'
 
 function App() {
-  const [ day, setDay ] = useState("DD")
-  const [ month, setMonth ] = useState("MM")
-  const [ year, setYear ] = useState("YYYY")
+  const [ day, setDay ] = useState("")
+  const [ month, setMonth ] = useState("")
+  const [ year, setYear ] = useState("")
 
   const handleSubmit = (e) => {
     e.preventDefault();
   };
 
+
   return (
     <div className="container">
       <form onSubmit={handleSubmit}>
       <label htmlFor="day" className="form-label">DAY</label>
-            <input type="text" name="day" id="day" onChange={e=>setDay(e.target.value)} value={day}/>
+            <input type="text" name="day" id="day" onChange={e=>setDay(e.target.value)} value={day} placeholder='DD'/>
 
             <label htmlFor="month" className="form-label">MONTH</label>
-            <input type="text" name="month" id="month" onChange={e=>setMonth(e.target.value)} value={month}/>
+            <input type="text" name="month" id="month" onChange={e=>setMonth(e.target.value)} value={month} placeholder='MM'/>
 
             <label htmlFor="year" className="form-label">YEAR</label>
-            <input type="text" name="year" id="year" onChange={e=>setYear(e.target.value)} value={year}/>
+            <input type="text" name="year" id="year" onChange={e=>setYear(e.target.value)} value={year} placeholder='YYYY'/>
         <button type="submit">
           <img src={icon_arrow} className="btn" alt="submit button" />
         </button>
