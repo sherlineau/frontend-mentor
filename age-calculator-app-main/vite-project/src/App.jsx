b0dd51c5ae983d5be0b2ddd11027d17d1055f17f
Sherline Au <17630357+sherlineau@users.noreply.github.com>
2024-01-07 17:06:18 -0600

    added onChange function to handle user input

--- age-calculator-app-main/vite-project/src/App.jsx
+++ age-calculator-app-main/vite-project/src/App.jsx
@@ -13,35 +13,14 @@ function App() {
   return (
     <div className="container">
       <form onSubmit={handleSubmit}>
-        <label htmlFor="day" className="form-label">
-          DAY
-        </label>
-        <input
-          type="text"
-          name="day"
-          id="day"
-          value={day}
-        />
+      <label htmlFor="day" className="form-label">DAY</label>
+            <input type="text" name="day" id="day" onChange={e=>setDay(e.target.value)} value={day}/>
 
-        <label htmlFor="month" className="form-label">
-          MONTH
-        </label>
-        <input
-          type="text"
-          name="month"
-          id="month"
-          value={month}
-        />
+            <label htmlFor="month" className="form-label">MONTH</label>
+            <input type="text" name="month" id="month" onChange={e=>setMonth(e.target.value)} value={month}/>
 
-        <label htmlFor="year" className="form-label">
-          YEAR
-        </label>
-        <input
-          type="text"
-          name="year"
-          id="year"
-          value={year}
-        />
+            <label htmlFor="year" className="form-label">YEAR</label>
+            <input type="text" name="year" id="year" onChange={e=>setYear(e.target.value)} value={year}/>
         <button type="submit">
           <img src={icon_arrow} className="btn" alt="submit button" />
         </button>
