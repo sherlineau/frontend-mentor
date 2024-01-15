66a40d070473ace03d375d87419c824102c06995
Sherline Au <17630357+sherlineau@users.noreply.github.com>
2024-01-15 11:24:37 -0600

    styling completed

--- advice-generator-app-main/vite-project/src/App.jsx
+++ advice-generator-app-main/vite-project/src/App.jsx
@@ -1,6 +1,9 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 import dice from './assets/images/icon-dice.svg'
+import './app.css'
+import mobile_divider from './assets/images/pattern-divider-mobile.svg'
+import desktop_divider from './assets/images/pattern-divider-desktop.svg'
 
 function App() {
   const [loading, setLoading] = useState(true);
@@ -24,10 +27,17 @@ function App() {
       {loading && <div>Loading...</div>}
       {!loading && (
         <div className="card">
-          <h1>ADVICE #{data.id}</h1>
-          {`"${data.advice}"`}
-          <div className="divider" />
-          <img src={dice} alt="random advice button" className="dice"/>
+          <p className="advice-id">ADVICE #{data.id}</p>
+          <p className="advice-text">{`"${data.advice}"`}</p>
+          <img 
+          src={desktop_divider}
+          srcSet={`${mobile_divider} 375px, ${desktop_divider} 1000px`}
+          sizes="(max-width: 375px) 375px, 1000px"
+          className="divider" 
+          />
+          <div className="dice">
+          <img src={dice} alt="random advice button" />
+          </div>
         </div>
       )}
     </div>
