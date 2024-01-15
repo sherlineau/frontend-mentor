2543e137b33cfd620672ee06b2a2eb91c352b5b4
Sherline Au <17630357+sherlineau@users.noreply.github.com>
2024-01-14 20:49:33 -0600

    on load, get advice from adviceslip api

--- advice-generator-app-main/vite-project/src/App.jsx
+++ advice-generator-app-main/vite-project/src/App.jsx
@@ -1,10 +1,33 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
 function App() {
+  const [loading, setLoading] = useState(true);
+  // data retrieved from api
+  const [data, setData] = useState([]);
+
+  // on load, get random advice from adviceslip api
+  useEffect(() => {
+    setLoading(true);
+    axios
+      .get(`https://api.adviceslip.com/advice`)
+      .then((res) => {
+        setData(res.data["slip"]);
+      })
+      .catch((err) => console.log(err));
+    setLoading(false);
+  }, []);
 
   return (
-    <>
-
-    </>
-  )
+    <div className="container">
+      {loading && <div>Loading...</div>}
+      {!loading && 
+      <div className="card">
+        <h1>ADVICE #{data.id}</h1>
+        {`"${data.advice}"`}
+        </div>}
+    </div>
+  );
 }
 
-export default App
+export default App;
